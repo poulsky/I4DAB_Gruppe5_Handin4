170dc301f9f4a21e0ad2329637734d2f010bea87
holmeren <soren_kh@hotmail.com>
2016-11-29 09:45:19 +0100

    HandIn4 projekt

---
+++ HandIn4/HandIn4/Model/Appartmentcharacteristic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandIn4.Model
+{
+    class Appartmentcharacteristic
+    {
+        public int No { get; set; }
+        public float Size { get; set; }
+        public int Floor { get; set; }
+        public int appartmentId { get; set; }
+    }
+}
---
+++ HandIn4/HandIn4/Model/Reading.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandIn4.Model
+{
+    class Reading
+    {
+        public int sensorId { get; set; }
+        public int appartmentId { get; set; }
+        public float value { get; set; }
+        public DateTime timestamp { get; set; }
+    }
+}
