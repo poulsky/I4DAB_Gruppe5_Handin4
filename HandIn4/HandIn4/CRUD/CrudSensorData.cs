c2120d44270319a7202455363824dbbe5584d74b
holmeren <soren_kh@hotmail.com>
2016-12-05 11:59:37 +0100

    CrudSensorData

--- HandIn4/HandIn4/CRUD/CrudSensorData.cs
+++ HandIn4/HandIn4/CRUD/CrudSensorData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,19 +11,55 @@ using HandIn4.Models;
 
 namespace HandIn4.CRUD
 {
-    class CrudSensorData
+   public class CrudSensorData
     {
 
         public void Create(SensorData data)
         {
             var db = new Context();
-            foreach (var reading in data.reading)
-            {
-                db.Readings.Add(reading);
-            }
-            db.SaveChanges();
+            //foreach (var reading in data.reading)
+            //{
+            //    db.Readings.Add(reading);
+            //}
 
             db.SensorDatas.Add(data);
+            db.SaveChanges();
+
+            
         }
+
+        public SensorData Read(int id)
+        {
+            var db = new Context();
+            var dataToRead = db.SensorDatas.Find(id);
+            
+                return dataToRead;
+            
+            
+        }
+
+        public void Update(int id)
+        {
+            var db = new Context();
+            var dataToUpdate = db.SensorDatas.Find(id);
+            
+     
+            
+
+        }
+
+        public void Delete(int id)
+        {
+            var db = new Context();
+            var dataToDelete = db.SensorDatas.Find(id);
+            if (dataToDelete != null)
+            {
+                db.SensorDatas.Remove(dataToDelete);
+                db.SaveChanges();
+            }
+        }
+
     }
+
+    
 }
