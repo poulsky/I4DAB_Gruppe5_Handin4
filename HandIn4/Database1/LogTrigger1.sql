82afb85a822c3e3ef258fa03642eccebf2880662
Mikkel <20112893@post.au.dk>
2016-12-06 10:26:28 +0100

    Flere triggers

---
+++ HandIn4/Database1/LogTrigger1.sql
@@ -0,0 +1,18 @@
+﻿CREATE TRIGGER [LogTrigger1]
+	ON [dbo].[Readings]
+	FOR DELETE
+	AS
+	DECLARE @dateTime DATETIME
+	DECLARE @apId INT
+	DECLARE @reId INT
+
+	SELECT @dateTime = (SELECT timestamp FROM deleted)
+	SELECT @apId = (SELECT appartmentId from deleted)
+	SELECT @reId = (SELECT ReadingId from deleted)
+
+	
+	BEGIN
+		SET NOCOUNT ON
+		INSERT INTO dbo.LogReadings (DateTime, Operations, apartmentId, ReadingId) 
+		VALUES (@dateTime, 'DELETE', @apId,@reId)  
+	END
