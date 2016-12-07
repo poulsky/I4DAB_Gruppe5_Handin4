3e2b8a2d7b93fd4217df96915c1cde70a04affe7
holmeren <soren_kh@hotmail.com>
2016-12-07 12:05:19 +0100

    View

---
+++ HandIn4/Database1/ViewMetaData.sql
@@ -0,0 +1,6 @@
+﻿CREATE VIEW [dbo].[MetaData]
+	AS SELECT Readings.timestamp, Readings.value, Sensorcharacteristics.unit, Sensorcharacteristics.description
+	FROM
+	 dbo.Readings INNER JOIN dbo.Sensorcharacteristics
+	  ON dbo.Readings.sensorId = dbo.Sensorcharacteristics.sensorId
+	  GO
