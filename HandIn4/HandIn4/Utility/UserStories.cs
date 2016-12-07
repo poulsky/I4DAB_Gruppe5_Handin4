2560776ed0246e41ca36a8f4a036cbfed7c7c081
Slaskefar <jeppebenjaminsen@hotmail.com>
2016-12-07 15:35:48 +0100

    resten af UserStories.cs skrevet

--- HandIn4/HandIn4/Utility/UserStories.cs
+++ HandIn4/HandIn4/Utility/UserStories.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -49,6 +50,60 @@ namespace HandIn4.Utility
 
                 return sensorValue;
             }
+        }
+
+        public Appartmentcharacteristic GetApartmentData(int apartmentId)
+        {
+            var db = new Context();
+            
+            return db.Appartmentcharacteristics.Find(apartmentId);
+        }
+
+        public int GetNumberOfApartmentsInSystem()
+        {
+            var db = new Context();
+            
+            return db.Appartmentcharacteristics.Count();
+
+        }
+
+        public float GetCurrentHeatEnergyConsumption(int apartmentId)
+        {
+            var db = new Context();
+
+            var ap = db.Readings.Where(t => t.appartmentId == apartmentId);
+            List<int> Id = new List<int>();
+            foreach (var reading in ap)
+            {
+                Id.Add(reading.sensorId);
+            }
+
+            int sensorId = 0;
+            foreach (var item in Id)
+            {
+                var sensor =
+                    db.Sensorcharacteristics.SingleOrDefault(
+                        t => t.sensorId == item && t.description == "(*Instant  Heat Energy consumption W*)");
+                if (sensor != null)
+                {
+                    sensorId = sensor.sensorId;
+                    break;
+                }
+            }
+
+            var re = db.Readings.Where(t => t.sensorId == sensorId);
+            DateTime time = DateTime.MinValue;
+            float sensorValue = -1;
+            foreach (var reading in re)
+            {
+                if (time < reading.timestamp)
+                {
+                    sensorValue = reading.value;
+                    time = reading.timestamp;
+                }
+            }
+            
+            return sensorValue;
         }
     }
 }
